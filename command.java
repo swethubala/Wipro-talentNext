8cc4fddef364169eeb79f1be7ddb277436f476fe
swethubala <65902288+swethubala@users.noreply.github.com>
2020-05-30 15:12:45 +0530

    Create command.java

---
+++ command.java
@@ -0,0 +1,11 @@
+public class CommandLine
+{
+    public static void main(String[] args)
+    {
+        System.out.print("Hi");
+        System.out.print(args[0]+",");
+        System.out.print(args[1]+"");
+        System.out.println("Welcome John");
+    }
+}
+© 2020 GitHub, Inc.
